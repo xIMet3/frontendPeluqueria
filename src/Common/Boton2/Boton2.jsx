d4b7a9d0e9a1b04f0af8a210b1f4d68715d324f2
xIMet3 <joaquin19_91@hotmail.com>
2023-07-25 02:46:44 +0200

    [feat]Creation of InputText and Button2 components. Replaced all the buttons of the navbar by the Boton2 component. // Creacion de los componentes InputText y Boton2. Reemplazados todos los botones de la navbar por el componente Boton2.

---
+++ src/Common/Boton2/Boton2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Boton2.css";
+import { useNavigate } from "react-router-dom";
+
+export const Boton2 = ({ path, name }) => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="boton2Diseño" onClick={() => navigate(path)}>
+            {name}
+
+        </div>
+    )
+}
--- src/Common/Header/Header.jsx
+++ src/Common/Header/Header.jsx
@@ -5,6 +5,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Boton1 } from "../Boton1/Boton1";
+import { Boton2 } from "../Boton2/Boton2";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -15,24 +16,32 @@ export const Header = () => {
     <Navbar collapseOnSelect expand="lg" className="bg-dark navbar-dark">
   <Container>
     <Navbar.Brand >
-      <Boton1 path={"/"} name={"Johnson Exclusive"}  />
+      <Boton1 path={"/"} name={"Johnson Exclusive"} />
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="#features" className="text-light">Features</Nav.Link>
-        <Nav.Link href="#pricing" className="text-light">Pricing</Nav.Link>
-        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <Nav.Link className="text-light">
+          <Boton2 path={"/"} name={"Features"} />
+        </Nav.Link>
+        <Nav.Link className="text-light">
+          <Boton2 path={"/"} name={"Pricing"} />
+        </Nav.Link>
+      </Nav>
+      <Nav>
+        <Nav.Link className="text-light">
+          <Boton2 path={"/"} name={"Iniciar sesión"} />
+        </Nav.Link>
+        <Nav.Link className="text-light">
+          <Boton2 path={"/"} name={"Regístrate"} />
+        </Nav.Link>
+        {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
           <NavDropdown.Divider />
           <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-      <Nav>
-        <Nav.Link href="#deets" className="text-light">More deets</Nav.Link>
-        <Nav.Link eventKey={2} href="#memes" className="text-light">Dank memes</Nav.Link>
+        </NavDropdown> */}
       </Nav>
     </Navbar.Collapse>
   </Container>
