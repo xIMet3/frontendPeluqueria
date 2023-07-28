2cadb0b48f563d7cc7510a6a27c84d1baa87a643
xIMet3 <joaquin19_91@hotmail.com>
2023-07-28 14:18:00 +0200

    [update]Modified in panelEmployed to make the design fully responsive. // Modificado en panelEmpleado para que el diseño sea totalmente responsive.

--- src/Pages/PanelEmpleado/PanelEmpleado.jsx
+++ src/Pages/PanelEmpleado/PanelEmpleado.jsx
@@ -6,9 +6,9 @@ import { todasLasCitas } from "../../../Services/apiCalls";
 export const PanelEmpleado = () => {
   const [citas, setCitas] = useState([]);
   const [dropdownStates, setDropdownStates] = useState({});
-  const [filterDate, setFilterDate] = useState(""); // Nuevo estado para el filtro de fecha
+  const [filterDate, setFilterDate] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const citasPerPage = 20; // Número de citas por página
+  const citasPerPage = 20;
 
   const token = useSelector((state) => state.usuario.credentials.token);
 
@@ -40,13 +40,12 @@ export const PanelEmpleado = () => {
 
   const filtrarCitasPorFecha = (cita) => {
     if (!filterDate) {
-      return true; // Mostrar todas las citas si no hay filtro de fecha
+      return true;
     }
 
-    // Formatear la fecha de la cita para que coincida con el formato del filtro
     const fechaCita = new Date(cita.fecha);
     const diaCita = fechaCita.getDate();
-    const mesCita = fechaCita.getMonth() + 1; // Los meses en JavaScript son indexados desde 0
+    const mesCita = fechaCita.getMonth() + 1;
     const anioCita = fechaCita.getFullYear();
     const fechaFormateada = `${anioCita}-${mesCita
       .toString()
@@ -57,7 +56,7 @@ export const PanelEmpleado = () => {
 
   const handleChangeFilter = (event) => {
     setFilterDate(event.target.value);
-    setCurrentPage(1); // Volver a la primera página al cambiar el filtro de fecha
+    setCurrentPage(1);
   };
 
   const handleNextPage = () => {
@@ -74,10 +73,7 @@ export const PanelEmpleado = () => {
   const indexOfLastCita = currentPage * citasPerPage;
   const indexOfFirstCita = indexOfLastCita - citasPerPage;
   const citasFiltradas = citas.filter(filtrarCitasPorFecha);
-  const citasPaginadas = citasFiltradas.slice(
-    indexOfFirstCita,
-    indexOfLastCita
-  );
+  const citasPaginadas = citasFiltradas.slice(indexOfFirstCita, indexOfLastCita);
 
   return (
     <div className="vistaEmpleadoEntera">
@@ -91,95 +87,92 @@ export const PanelEmpleado = () => {
           onChange={handleChangeFilter}
         />
       </div>
-      <table className="citas-table">
-        <thead>
-          <tr>
-            <th>Fecha</th>
-            <th>Hora</th>
-            <th>Usuario</th>
-            <th>Teléfono</th>
-            <th>Empleado</th>
-            <th>Servicio</th>
-            <th>Precio</th>
-            <th>Comentario</th>
-            <th>Estado</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {citasFiltradas.length > 0 ? (
-            citasPaginadas.map((cita) => {
-              const { fechaLocal, horaLocal } = formatearFecha(cita.fecha);
-              const nombreCompleto = `${cita.Usuario?.nombre} ${cita.Usuario?.apellido}`;
-              const estiloEstado =
-                cita.Cita_estado?.nombre_cita_estado === "Pendiente"
-                  ? { color: "orange" }
-                  : {};
+      <div className="table-responsive">
+        <table className="table table-bordered citas-table">
+          <thead>
+            <tr>
+              <th>Fecha</th>
+              <th>Hora</th>
+              <th>Usuario</th>
+              <th>Teléfono</th>
+              <th>Empleado</th>
+              <th>Servicio</th>
+              <th>Precio</th>
+              <th>Comentario</th>
+              <th>Estado</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {citasFiltradas.length > 0 ? (
+              citasPaginadas.map((cita) => {
+                const { fechaLocal, horaLocal } = formatearFecha(cita.fecha);
+                const nombreCompleto = `${cita.Usuario?.nombre} ${cita.Usuario?.apellido}`;
+                const estiloEstado =
+                  cita.Cita_estado?.nombre_cita_estado === "Pendiente"
+                    ? { color: "orange" }
+                    : {};
 
-              return (
-                <React.Fragment key={cita.id}>
-                  <tr>
-                    <td>{fechaLocal}</td>
-                    <td>{horaLocal}</td>
-                    <td>{nombreCompleto}</td>
-                    <td>{cita.Usuario?.telefono}</td>
-                    <td>{cita.Empleado?.nombre}</td>
-                    <td>{cita.Servicio?.nombre_servicio}</td>
-                    <td>{cita.Servicio?.precio_servicio}</td>
-                    <td>{cita.comentario}</td>
-                    <td style={estiloEstado}>
-                      {cita.Cita_estado?.nombre_cita_estado}
-                    </td>
-                    <td>
-                      <button
-                        onClick={() => toggleDropdown(cita.id)}
-                        id="botonOpciones"
-                      >
-                        Opciones
-                      </button>
-                    </td>
-                  </tr>
-                  {/* Desplegable con botones */}
-                  {dropdownStates[cita.id] && (
+                return (
+                  <React.Fragment key={cita.id}>
                     <tr>
-                      <td colSpan="10">
-                        {/* Aquí utilizamos colSpan para que el contenedor de botones ocupe todas las columnas */}
-                        <div className="botones-desplegable">
-                          <button
-                            id="botonModificar"
-                            onClick={() => console.log("Modificar", cita.id)}
-                          >
-                            Modificar
-                          </button>
-                          <button
-                            id="botonCancelar"
-                            onClick={() => console.log("Cancelar", cita.id)}
-                          >
-                            Cancelar
-                          </button>
-                          <button
-                            id="botonRealizada"
-                            onClick={() => console.log("Realizada", cita.id)}
-                          >
-                            Realizada
-                          </button>
-                        </div>
+                      <td>{fechaLocal}</td>
+                      <td>{horaLocal}</td>
+                      <td>{nombreCompleto}</td>
+                      <td>{cita.Usuario?.telefono}</td>
+                      <td>{cita.Empleado?.nombre}</td>
+                      <td>{cita.Servicio?.nombre_servicio}</td>
+                      <td>{cita.Servicio?.precio_servicio}</td>
+                      <td>{cita.comentario}</td>
+                      <td style={estiloEstado}>
+                        {cita.Cita_estado?.nombre_cita_estado}
+                      </td>
+                      <td>
+                        <button
+                          onClick={() => toggleDropdown(cita.id)}
+                          id="botonOpciones"
+                        >
+                          Opciones
+                        </button>
                       </td>
                     </tr>
-                  )}
-                </React.Fragment>
-              );
-            })
-          ) : (
-            // Mostrar mensaje si no hay citas filtradas
-            <tr>
-              <td colSpan="10">
-                No se encontraron citas para la fecha seleccionada.
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+                    {dropdownStates[cita.id] && (
+                      <tr>
+                        <td colSpan="10">
+                          <div className="botones-desplegable">
+                            <button
+                              id="botonModificar"
+                              onClick={() => console.log("Modificar", cita.id)}
+                            >
+                              Modificar
+                            </button>
+                            <button
+                              id="botonCancelar"
+                              onClick={() => console.log("Cancelar", cita.id)}
+                            >
+                              Cancelar
+                            </button>
+                            <button
+                              id="botonRealizada"
+                              onClick={() => console.log("Realizada", cita.id)}
+                            >
+                              Realizada
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    )}
+                  </React.Fragment>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan="10">No se encontraron citas para la fecha seleccionada.</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
       <div className="pagination">
         {currentPage > 1 && <button onClick={handlePrevPage}>Anterior</button>}
         {citasFiltradas.length > citasPerPage &&
