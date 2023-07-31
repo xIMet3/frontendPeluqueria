8f46d517f1ae27bb7e71866ca926a6ac96ca35c9
xIMet3 <joaquin19_91@hotmail.com>
2023-07-31 03:00:49 +0200

    [update]Modified a detail of the Services view design. // Modificado un detalle del diseño de la vista Servicios.

--- src/Pages/Servicios/Servicios.jsx
+++ src/Pages/Servicios/Servicios.jsx
@@ -4,34 +4,26 @@ import { mostrarServicios } from "../../../Services/apiCalls";
 
 export const Servicios = () => {
   const [servicios, setServicios] = useState([]);
-  const [selectedServiceId, setSelectedServiceId] = useState(null);
+  const [servicioSeleccionado, setServicioSeleccionado] = useState(null);
 
   useEffect(() => {
-    async function fetchServicios() {
+    async function busquedaSevicios() {
       try {
-        const response = await mostrarServicios();
-        if (response.success) {
-          setServicios(response.data);
+        const res = await mostrarServicios();
+        if (res.success) {
+          setServicios(res.data);
         } else {
-          console.error("Error al obtener los servicios:", response.message);
+          console.error("Error al obtener los servicios:", res.message);
         }
       } catch (error) {
         console.error("Error al obtener los servicios:", error);
       }
     }
-    fetchServicios();
+    busquedaSevicios();
   }, []);
 
-  const openModal = (serviceId) => {
-    setSelectedServiceId(serviceId);
-  };
-
-  const closeModal = () => {
-    setSelectedServiceId(null);
-  };
-
   return (
-    <div className="serviciosBackground">
+    <div className="fondoServicios">
       <div className="serviciosEntera">
         {servicios.map((servicio) => (
           <button key={servicio.id} className="servicioItem">
@@ -42,12 +34,12 @@ export const Servicios = () => {
         ))}
       </div>
 
-      {selectedServiceId && (
+      {servicioSeleccionado && (
         <div className="custom-modal">
           <div className="modal-content">
             {servicios.map(
               (servicio) =>
-                servicio.id === selectedServiceId && (
+                servicio.id === servicioSeleccionado && (
                   <div key={servicio.id} id="modalIndividual">
                     <h2>{servicio.nombre_servicio}</h2>
                     <p>Precio: {servicio.precio_servicio}</p>
